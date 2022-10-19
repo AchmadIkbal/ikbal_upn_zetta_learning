e2d659e2e884aa69ed82ddcea2a5008e6ad8c390
Cyberlol <achmad.ikbal69@gmail.com>
2022-10-19 20:07:19 +0700

    edit

---
+++ input.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZettaXUPNVYK_HTML</title>
+    <link rel="icon" href="./logo_upn.png" type="image/x-icon">
+    
+ <!-- Bootstrap core CSS -->
+ <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        .center{
+            margin: auto;
+        }
+        .container{
+            background-color: cyan;
+            margin-left: 150px;
+            margin-right: 150px;
+            padding-top: 25px;
+            padding-bottom: 25px;
+            border-radius: 30px;
+            border: 5px solid;
+            padding: 10px;
+        
+        }
+    </style>
+    <body>
+        <div class="container">
+            <h1>Input Item</h1>
+        </div>
+    </body>
