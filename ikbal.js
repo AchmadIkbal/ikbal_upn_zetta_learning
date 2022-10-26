52e9fc63f8a24b4a241147f2e0ef5453cf3069e4
Cyberlol <achmad.ikbal69@gmail.com>
2022-10-26 10:34:37 +0700

    finally

--- ikbal.js
+++ ikbal.js
@@ -1,24 +1,24 @@
-function priceCalculator(book, discount, tax){
+function purchasingBook(book, percentageDiscount, percentageTax) {
     let result = 0;
-    if(book && book.price){
-        const price=book.price;
-        const discountAmount=price*(discount/100);
-        const priceAfterDiscount=price - discountAmount;
-        const taxAmount=priceAfterDiscount*(tax/100);
-        const priceAfterTax=priceAfterDiscount+taxAmount;
+    if (book && book.price) {
+        const price = book.price;
+        const discountAmount = price * (percentageDiscount / 100);
+        const priceAfterDiscount = price - discountAmount;
+        const taxAmount = priceAfterDiscount * (percentageTax / 100);
+        const priceAfterTax = priceAfterDiscount + taxAmount;
 
         console.group();
         console.log("Judul Buku : ", book.title);
-        console.log("Harga Buku : ",price);
-        console.log("Persentase diskon : ",discount,"%");
-        console.log("Biaya setelah diskon : ",priceAfterDiscount);
-        console.log("Persantase PPN : ",tax,"%");
-        console.log("Biaya PPN : ",taxAmount);
-        console.log("Total pembayaran : ",priceAfterTax);
+        console.log("Harga Buku : ", price);
+        console.log("Persentase diskon : ", percentageDiscount, "%");
+        console.log("Biaya setelah diskon : ", priceAfterDiscount);
+        console.log("Persentase PPN : ", percentageTax, "%");
+        console.log("Biaya PPN : ", taxAmount);
+        console.log("Total pembayaran : ", priceAfterTax);
         console.groupEnd();
     }
 
     return 0;
 }
 
-priceCalculator({title: "JS For baby", price: 125000, status: true},50,10);
+purchasingBook({ title: "JS Ikbal", price: 100000, status: true }, 50, 10);
