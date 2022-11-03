7e1fbf05d71eea755b4fe6e337eb777cae218dfd
Cyberlol <achmad.ikbal69@gmail.com>
2022-11-04 00:20:36 +0700

    bismillah

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
-import { ItemsInputComponent } from './items-input/items-input.component';
+import { ItemsComponent } from './task/task.component';
+import { ItemsInputComponent } from './task-input/task-input.component';
+import { TaskComponent } from './task/task.component';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ItemsInputComponent
+    ItemsInputComponent,
+    TaskComponent,
+    TaskInputComponent
   ],
   imports: [
     BrowserModule,
