7fcc1ad20fd726f04b34bbf42a546e03cf78f045
Cyberlol <achmad.ikbal69@gmail.com>
2022-11-12 02:12:12 +0700

    day 5

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KasirModule } from './kasir/kasir.module';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule,
