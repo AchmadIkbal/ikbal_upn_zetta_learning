e7e60cfc7ea0ffe6c49fe00c10440cba48450265
Cyberlol <achmad.ikbal69@gmail.com>
2022-11-11 13:47:09 +0700

    dayy4

--- angular_learning/src/app/jualbeli/menu/menu.component.ts
+++ angular_learning/src/app/jualbeli/menu/menu.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { item } from '../kasir/kasir.component'
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
+  
 })
 export class MenuComponent implements OnInit {
 
@@ -12,4 +14,11 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  @Input('items') items: item[]=[]
+  @Output ('itemAdded') onAddItem : EventEmitter<item> = new EventEmitter<item>()
+
+  addItem(item :item) : void{
+    this.onAddItem.emit(item)
+  }
 }
