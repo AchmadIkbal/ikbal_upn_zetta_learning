a6ae474ea2238f519757bf13e3773cc5bf4f8e2c
Cyberlol <achmad.ikbal69@gmail.com>
2022-11-18 10:58:38 +0700

    day6

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ServicesComponent } from './services/services.component';
+import { ItemDetailComponent } from './item/item-detail/item-detail.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ServicesComponent,
+    ItemDetailComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
